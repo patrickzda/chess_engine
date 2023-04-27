615fcc6097d8f34d8940061e548d49732bde103e
entspannTOM <moottemc@gmail.com>
2023-04-27 17:24:28 +0200

    MoveMasks: indizierung der diagonalen geändert, ACHTUNG,so ist es noch falsch!, rays Funktion hinzugefügt MoveMasksTest: erweitert für rays

--- src/engine/move_generation/MoveMasks.java
+++ src/engine/move_generation/MoveMasks.java
@@ -1,5 +1,7 @@
 package engine.move_generation;
 
+import engine.representation.Direction;
+
 public class MoveMasks {
     /*
     Wir benötigen jeweils Zugmasken für alle möglichen Richtungen, in welche die Läufer, die Türme und die Damen ziehen können.
@@ -33,14 +35,80 @@ public class MoveMasks {
             // für die vertikalen 72340172838076673L (einer vertikale reihe aus einsen) um 0-7 shiften
             verticals[i] = (72340172838076673L << i);
 
-            diagonals[i] = (72624976668147840L >>> (8 * (7 - i)));
-            antiDiagonals[i] = (-9205322385119247871L >>> (8 * (7 - i)));
+
+            diagonals[i] = (72624976668147840L << (8 * i) + 8);
+            antiDiagonals[i] = (-9205322385119247871L << (8 * i) + 8);
         }
 
         for (int j = 8; j < 15; j++) {
-            diagonals[j] = (72624976668147840L << (8 * j) + 8);
-            antiDiagonals[j] = (-9205322385119247871L << (8 * j) + 8);
+            diagonals[j] = (72624976668147840L >>> (8 * (7 - j)));
+            antiDiagonals[j] = (-9205322385119247871L >>> (8 * (7 - j)));
         }
+    }
+
+    public long rays(Direction dir, int index) {
+        long temp;
+        switch(dir) {
+            case NORTH:
+                System.out.println("NORTH, " + index);
+                temp = -9223372036854775808L >> (62 - index);
+
+                return verticals[index % 8] & temp;
+
+            case NORTH_EAST:
+                System.out.println("NORTH_EAST, " + index);
+                temp = -9223372036854775808L >> (62 - index);
+                int rank = index / 8;
+                int file = index % 8;
+                return antiDiagonals[(rank - file) & 15];
+
+            case EAST:
+                System.out.println("EAST, " + index);
+                break;
+            case SOUTH_EAST:
+                System.out.println("SOUTH_EAST, " + index);
+
+                temp = ~(-9223372036854775808L >> (63 - index));
+                return diagonals[index % 8] & temp;
+
+            case SOUTH:
+                System.out.println("SOUTH, " + index);
+
+                temp = ~(-9223372036854775808L >> (63 - index));
+                return verticals[index % 8] & temp;
+
+            case SOUTH_WEST:
+                System.out.println("SOUTH_WEST, " + index);
+
+                temp = ~(-9223372036854775808L >> (63 - index));
+                return antiDiagonals[index % 8] & temp;
+
+            case WEST:
+                System.out.println("WEST, " + index);
+                break;
+
+            case NORTH_WEST:
+                System.out.println("NORTH_WEST, " + index);
+
+                temp = -9223372036854775808L >> (62 - index);
+                return diagonals[index % 8] & temp;
+
+            default:
+                System.out.println("irgendwas ist gewaltig schief gelaufen...");
+                break;
+
+        }
+
+
+
+
+
+
+
+
+
+
+        return 0L;
     }
 
     public static void printBitBoard(long bitBoard) {
--- src/test/MoveMasksTest.java
+++ src/test/MoveMasksTest.java
@@ -3,6 +3,7 @@ package test;
 import engine.move_generation.MoveMasks;
 import org.junit.jupiter.api.Test;
 
+import static engine.representation.Direction.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoveMasksTest {
@@ -11,27 +12,44 @@ class MoveMasksTest {
         System.out.println("Starte Test für MoveMasks");
         MoveMasks m = new MoveMasks();
 
-        for (int i = 0; i < 8; i++) {
-            System.out.println("Horizontale " + i + ":");
-            long h = m.horizontals[i];
-
-            MoveMasks.printBitBoard(h);
-
-            System.out.println("Vertikale " + i + ":");
-            long v = m.verticals[i];
-            MoveMasks.printBitBoard(v);
-        }
+//        for (int i = 0; i < 8; i++) {
+//            System.out.println("Horizontale " + i + ":");
+//            long h = m.horizontals[i];
+//
+//            MoveMasks.printBitBoard(h);
+//
+//            System.out.println("Vertikale " + i + ":");
+//            long v = m.verticals[i];
+//            MoveMasks.printBitBoard(v);
+//        }
 
         for (int i = 0; i < 15; i++) {
-            System.out.println("Hauptdiagonale " + i + ":");
-            long md = m.diagonals[i];
-
-            MoveMasks.printBitBoard(md);
+//            System.out.println("Hauptdiagonale " + i + ":");
+//            long md = m.diagonals[i];
+//
+//            MoveMasks.printBitBoard(md);
 
             System.out.println("Nebendiagonale " + i + ":");
             long sd = m.antiDiagonals[i];
             MoveMasks.printBitBoard(sd);
         }
 
+    }
+
+    @Test
+    public void rays() {
+        MoveMasks m = new MoveMasks();
+        MoveMasks.printBitBoard(m.rays(NORTH, 27));
+        System.out.println();
+        MoveMasks.printBitBoard(m.rays(SOUTH, 27));
+        System.out.println();
+        MoveMasks.printBitBoard(m.rays(NORTH_EAST, 27));
+        System.out.println();
+        MoveMasks.printBitBoard(m.rays(SOUTH_EAST, 27));
+        System.out.println();
+        MoveMasks.printBitBoard(m.rays(NORTH_WEST, 27));
+        System.out.println();
+        MoveMasks.printBitBoard(m.rays(SOUTH_WEST, 27));
+        System.out.println();
     }
 }
