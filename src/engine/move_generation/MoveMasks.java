58b4442708da7d28bd39b3b478b2343592f0b018
entspannTOM <moottemc@gmail.com>
2023-04-29 16:48:07 +0200

    MoveMasks: die Rays-Funktion funktioniert jetzt für alle Indizes und Richtungen. NEU: allDirections(int index) gibt die Zugmöglichkeit in alle Richtungen aus MoveMasksTest: Test für Rays fertig mit Assert-Statements, Test für allDirections noch ohne Assert-Statement

--- src/engine/move_generation/MoveMasks.java
+++ src/engine/move_generation/MoveMasks.java
@@ -2,6 +2,8 @@ package engine.move_generation;
 
 import engine.representation.Direction;
 
+import static engine.representation.Direction.*;
+
 public class MoveMasks {
     /*
     Wir benötigen jeweils Zugmasken für alle möglichen Richtungen, in welche die Läufer, die Türme und die Damen ziehen können.
@@ -22,96 +24,186 @@ public class MoveMasks {
     Die einzelnen Bitmasken werden als long abgespeichert und müssen in irgendeine schlaue Datenstruktur eingefügt werden, sodass man schnell auf sie zugreifen kann.
      */
 
-    public long[] diagonals = new long[15];
-    public long[] antiDiagonals = new long[15];
+    public long[] diagonals = new long[16];
+    public long[] antiDiagonals = new long[16];
     public long[] horizontals = new long[8];
     public long[] verticals = new long[8];
+
+    private long DIAGONAL = -9205322385119247871L;
+    private long ANTI_DIAGONAL = 72624976668147840L;
+    private long MSB_ONE = -9223372036854775808L;
+    private long HORIZONTAL = 255L;
+    private long VERTICAL = 72340172838076673L;
+    private long ZEROS = 0L;
     public MoveMasks() {
         // horizontalen und vertikalen errechnen
         for (int i = 0; i < 8; i++) {
             // für die Horizontalen 255 (0b11111111) um ein vierlfaches von 8 shiften
-            horizontals[i] = (255L << i * 8);
+            horizontals[i] = (HORIZONTAL << i * 8);
 
             // für die vertikalen 72340172838076673L (einer vertikale reihe aus einsen) um 0-7 shiften
-            verticals[i] = (72340172838076673L << i);
+            verticals[i] = (VERTICAL << i);
 
-
-            diagonals[i] = (72624976668147840L << (8 * i) + 8);
-            antiDiagonals[i] = (-9205322385119247871L << (8 * i) + 8);
+            // für die Diagonalen wird einfach die haupt- bzw. anti-Diagonale geshiftet
+            diagonals[i] = (DIAGONAL << (8 * i));
+            antiDiagonals[i] = (ANTI_DIAGONAL >>> (8 * i));
         }
 
-        for (int j = 8; j < 15; j++) {
-            diagonals[j] = (72624976668147840L >>> (8 * (7 - j)));
-            antiDiagonals[j] = (-9205322385119247871L >>> (8 * (7 - j)));
+        // aus Gründen der Diagonal-Berechnung aus dem Index muss die 9. Diagonale (index 8) leer sein
+        diagonals[8] = ZEROS;
+        antiDiagonals[8] = ZEROS;
+
+        for (int j = 9; j < 16; j++) {
+            // Diagonalen auch in die andere Richtung shiften, da es insgesamt 15 diagonalen gibt
+            // (16, wenn man den Index 8 mitzählt)
+            diagonals[j] = (DIAGONAL >>> (8 * (8 - j)));
+            antiDiagonals[j] = (ANTI_DIAGONAL << (8 * (8 - j)));
         }
+    }
+
+    private long maskNorth(int index) {
+        // Bitmaske, die nur in nördlicher Richtung von dem Ausgangsindex einsen hat
+
+        // ein Edge-Case muss abgefangen werden, wenn das Feld das MSB ist, sonst wird die Maske auf nur einsen
+        // gesetzt und die Richtung wird nicht abgeschnitten.
+        // TODO: evtl. bessere Lösung ohne if-Statement finden
+        if (index == 63) {
+            return 0L;
+        }
+
+        return MSB_ONE >> (62 - index);
+    }
+
+    private long maskEast(int file) {
+        // Bitmaske, die nur in östlicher Richtung von dem Ausgangsindex einsen hat
+
+        // ein Edge-Case muss abgefangen werden, wenn das Feld am östlichen Rand liegt, sonst wird das
+        // Feld auf dem der Spielstein steht auch als mögliches Zielfeld mit ausgegeben
+        // TODO: evtl. bessere Lösung ohne if-Statement finden
+        if (file == 7) {
+            return 0L;
+        }
+
+        long mask = MSB_ONE >> (6 - file);
+        mask = mask | (mask >>> 8);
+        mask = mask | (mask >>> 16);
+        return mask | (mask >>> 32);
+    }
+
+    private long maskSouth(int index) {
+        // Bitmaske, die nur in südlicher Richtung von dem Ausgangsindex einsen hat
+        return ~(MSB_ONE >> (63 - index));
+    }
+
+    private long maskWest(int file) {
+        // Bitmaske, die nur in westlicher Richtung von dem Ausgangsindex einsen hat
+        long mask = MSB_ONE >> (7 - file);
+        mask = mask | (mask >>> 8);
+        mask = mask | (mask >>> 16);
+        return ~(mask | (mask >>> 32));
     }
 
     public long rays(Direction dir, int index) {
-        long temp;
+        long mask, res;
+        int rank, file;
         switch(dir) {
             case NORTH:
-                System.out.println("NORTH, " + index);
-                temp = -9223372036854775808L >> (62 - index);
+                file = index & 7;   // = index % 8
+                mask = maskNorth(index);
 
-                return verticals[index % 8] & temp;
+                res = verticals[file] & mask;
+                break;
 
             case NORTH_EAST:
-                System.out.println("NORTH_EAST, " + index);
-                temp = -9223372036854775808L >> (62 - index);
-                int rank = index / 8;
-                int file = index % 8;
-                return antiDiagonals[(rank - file) & 15];
+                rank = index >> 3;  // = index / 8
+                file = index & 7;   // = index % 8
+                mask = maskNorth(index);
+
+                res = diagonals[(rank - file) & 15] & mask;
+                //              ^^^^^^^^^^^^^^^^^^
+                // Berechnet die gesuchte Diagonale
+                break;
 
             case EAST:
-                System.out.println("EAST, " + index);
-                break;
-            case SOUTH_EAST:
-                System.out.println("SOUTH_EAST, " + index);
+                rank = index >> 3;  // = index / 8
+                file = index & 7;   // = index % 8
+                mask = maskEast(file);
 
-                temp = ~(-9223372036854775808L >> (63 - index));
-                return diagonals[index % 8] & temp;
+                res = horizontals[rank] & mask;
+                break;
+
+            case SOUTH_EAST:
+                rank = index >> 3;  // = index / 8
+                file = index & 7;   // = index % 8
+                mask = maskSouth(index);
+
+                res = antiDiagonals[(rank + file) ^ 7] & mask;
+                //                  ^^^^^^^^^^^^^^^^^
+                // Berechnet die gesuchte anti-Diagonale
+                break;
 
             case SOUTH:
-                System.out.println("SOUTH, " + index);
+                file = index & 7;   // = index % 8
+                mask = maskSouth(index);
 
-                temp = ~(-9223372036854775808L >> (63 - index));
-                return verticals[index % 8] & temp;
+                res = verticals[file] & mask;
+                break;
 
             case SOUTH_WEST:
-                System.out.println("SOUTH_WEST, " + index);
+                rank = index >> 3;  // = index / 8
+                file = index & 7;   // = index % 8
+                mask = maskSouth(index);
 
-                temp = ~(-9223372036854775808L >> (63 - index));
-                return antiDiagonals[index % 8] & temp;
+                res = diagonals[(rank - file) & 15] & mask;
+                //              ^^^^^^^^^^^^^^^^^^
+                // Berechnet die gesuchte Diagonale
+                break;
 
             case WEST:
-                System.out.println("WEST, " + index);
+                rank = index >> 3;  // = index / 8
+                file = index & 7;   // = index % 8
+
+                mask = maskWest(file);
+
+                res = horizontals[rank] & mask;
                 break;
 
             case NORTH_WEST:
-                System.out.println("NORTH_WEST, " + index);
+                rank = index >> 3;  // = index / 8
+                file = index & 7;   // = index % 8
 
-                temp = -9223372036854775808L >> (62 - index);
-                return diagonals[index % 8] & temp;
+                mask = maskNorth(index);
+
+                res = antiDiagonals[(rank + file) ^ 7] & mask;
+                //                  ^^^^^^^^^^^^^^^^^
+                // Berechnet die gesuchte anti-Diagonale
+                break;
 
             default:
+                // dieses Statement dürfte nie erreicht werden! Aus komplettheitsgründen wird in dem Fall
+                // eine Fehlermeldung ausgegeben und eine leere Bitmaske zurückgegeben
                 System.out.println("irgendwas ist gewaltig schief gelaufen...");
+                res = ZEROS;
                 break;
 
         }
+        return res;
+    }
 
-
-
-
-
-
-
-
-
-
-        return 0L;
+    public long allDirections(int index) {
+        // verodert alle Richtungen von einem Index
+        return rays(NORTH, index) |
+               rays(NORTH_EAST, index) |
+               rays(EAST, index) |
+               rays(SOUTH_EAST, index) |
+               rays(SOUTH, index) |
+               rays(SOUTH_WEST, index) |
+               rays(WEST, index) |
+               rays(NORTH_WEST, index);
     }
 
     public static void printBitBoard(long bitBoard) {
+        // für Debugging-Zwecke. Gibt das Bitboard in der Reihenfolge von einem Schachbrett aus
         for (int x = 0; x < 8; x++) {
             for (int y = 0; y < 8; y++) {
                 int i = (7 - x) * 8 + y;
--- src/test/MoveMasksTest.java
+++ src/test/MoveMasksTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import engine.move_generation.MoveMasks;
+import engine.representation.Direction;
 import org.junit.jupiter.api.Test;
 
 import static engine.representation.Direction.*;
@@ -12,22 +13,22 @@ class MoveMasksTest {
         System.out.println("Starte Test für MoveMasks");
         MoveMasks m = new MoveMasks();
 
-//        for (int i = 0; i < 8; i++) {
-//            System.out.println("Horizontale " + i + ":");
-//            long h = m.horizontals[i];
-//
-//            MoveMasks.printBitBoard(h);
-//
-//            System.out.println("Vertikale " + i + ":");
-//            long v = m.verticals[i];
-//            MoveMasks.printBitBoard(v);
-//        }
+        for (int i = 0; i < 8; i++) {
+            System.out.println("Horizontale " + i + ":");
+            long h = m.horizontals[i];
 
-        for (int i = 0; i < 15; i++) {
-//            System.out.println("Hauptdiagonale " + i + ":");
-//            long md = m.diagonals[i];
-//
-//            MoveMasks.printBitBoard(md);
+            MoveMasks.printBitBoard(h);
+
+            System.out.println("Vertikale " + i + ":");
+            long v = m.verticals[i];
+            MoveMasks.printBitBoard(v);
+        }
+
+        for (int i = 0; i < 16; i++) {
+            System.out.println("Hauptdiagonale " + i + ":");
+            long md = m.diagonals[i];
+
+            MoveMasks.printBitBoard(md);
 
             System.out.println("Nebendiagonale " + i + ":");
             long sd = m.antiDiagonals[i];
@@ -39,17 +40,61 @@ class MoveMasksTest {
     @Test
     public void rays() {
         MoveMasks m = new MoveMasks();
-        MoveMasks.printBitBoard(m.rays(NORTH, 27));
-        System.out.println();
-        MoveMasks.printBitBoard(m.rays(SOUTH, 27));
-        System.out.println();
-        MoveMasks.printBitBoard(m.rays(NORTH_EAST, 27));
-        System.out.println();
-        MoveMasks.printBitBoard(m.rays(SOUTH_EAST, 27));
-        System.out.println();
-        MoveMasks.printBitBoard(m.rays(NORTH_WEST, 27));
-        System.out.println();
-        MoveMasks.printBitBoard(m.rays(SOUTH_WEST, 27));
-        System.out.println();
+        Direction[] directions = {  NORTH, NORTH, NORTH, NORTH,
+                                    NORTH_EAST, NORTH_EAST, NORTH_EAST, NORTH_EAST,
+                                    EAST, EAST, EAST, EAST,
+                                    SOUTH_EAST, SOUTH_EAST, SOUTH_EAST, SOUTH_EAST,
+                                    SOUTH, SOUTH, SOUTH, SOUTH,
+                                    SOUTH_WEST, SOUTH_WEST, SOUTH_WEST, SOUTH_WEST,
+                                    WEST, WEST, WEST, WEST,
+                                    NORTH_WEST, NORTH_WEST, NORTH_WEST, NORTH_WEST
+                                 };
+        int[] indizes = {   27, 3, 63, 59,
+                            0, 35, 58, 63,
+                            16, 43, 39, 7,
+                            56, 10, 39, 7,
+                            58, 37, 1, 7,
+                            63, 34, 0, 5,
+                            39, 50, 56, 24,
+                            7, 19, 56, 63
+                        };
+        long[] results = {  578721382569869312L, 578721382704613376L, 0L, 0L,
+                            -9205322385119247872L, 4620710809868173312L, 0L, 0L,
+                            16646144L, 263882790666240L, 0L, 0L,
+                            567382630219904L, 8L, 0L, 0L,
+                            1130315200594948L, 538976288L, 0L, 0L,
+                            18049651735527937L, 33619968L, 0L, 0L,
+                            545460846592L, 844424930131968L, 0L, 0L,
+                            72624976668147712L, 1108168671232L, 0L, 0L
+
+                         };
+
+        for (int i = 0; i < results.length; i++) {
+            // Fehler einmal ausprinten, damit schnell zu sehen ist, wo der Fehler liegt und was falsch ist
+            if (results[i] != m.rays(directions[i], indizes[i])) {
+                System.out.println("FEHLER! Index: " + indizes[i] + "    Richtung: " + directions[i]);
+                System.out.println("Ergebnis: ");
+                MoveMasks.printBitBoard(m.rays(directions[i], indizes[i]));
+                System.out.println("Erwartet: ");
+                MoveMasks.printBitBoard((results[i]));
+            }
+            assertEquals(results[i], m.rays(directions[i], indizes[i]));
+
+        }
+    }
+
+    @Test
+    public void allDirections() {
+        MoveMasks m = new MoveMasks();
+        int[] indizes = {27, 0, 7, 56, 63, 5, 39, 60, 8};
+        int index;
+
+        for (int i = 0; i < indizes.length; i++) {
+            index = indizes[i];
+            System.out.println("index = " + index);
+            MoveMasks.printBitBoard(m.allDirections(index));
+            System.out.println();
+        }
+
     }
 }
