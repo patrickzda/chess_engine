42a85de0f77440604f1afd6091daa24dfd8582cb
unknown <adam.knothe@gmx.de>
2023-04-29 18:55:15 +0200

    perfomance

--- src/performance/MoveGeneratorPerformance.java
+++ src/performance/MoveGeneratorPerformance.java
@@ -1,29 +1,54 @@
 package performance;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MoveGeneratorPerformance {
 
     public void FunktionToTest(){
-        long total = 0;
+        fillArray();
+
+        /*long total = 0;
         for (long i = Integer.MIN_VALUE; i < Integer.MAX_VALUE* 2L; i++) {
             total += i;
 
-            /**/
+
         }
         for (int j = Integer.MIN_VALUE; j < Integer.MAX_VALUE; j++) {
             total += j;
+        }*/
+    }
+
+    public void fillArray(){
+        String arr[] = new String[100000000];
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = "hello_"+i ;
+        }
+    }
+    public void fillArrayList(){
+        ArrayList<String> arr = new ArrayList<String>();
+        for (int i = 0; i < 100000000; i++){
+            arr.add("hello_"+i);
         }
     }
 
     public void PerfomanceTime(int passes){
         long startTime = System.currentTimeMillis();
-        FunktionToTest();
+        fillArray();
         long stopTime = System.currentTimeMillis();
         long elapsedTime = stopTime - startTime;
-        System.out.println("First Time took: "+elapsedTime+" Milliseconds");
+        System.out.println("Array Firsttime took: "+elapsedTime+" Milliseconds");
         if(passes > 0) System.out.println("The Average is: " +AverageExecutionTime(passes));
+
+
+        startTime = System.currentTimeMillis();
+        fillArrayList();
+        stopTime = System.currentTimeMillis();
+        elapsedTime = stopTime - startTime;
+        System.out.println("ArrayList Firsttime took: "+elapsedTime+" Milliseconds");
+        if(passes > 0) System.out.println("The Average is: " +AverageExecutionTime(passes));
+
 
     }
 
