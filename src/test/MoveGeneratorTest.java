f6a9402e23586ac7178145322946dac26d00f02d
patrickzda <81521650+patrickzda@users.noreply.github.com>
2023-05-03 11:39:17 +0200

    MoveGeneratorTest: Test für isAttacked()

--- src/test/MoveGeneratorTest.java
+++ src/test/MoveGeneratorTest.java
@@ -19,27 +19,27 @@ public class MoveGeneratorTest {
         MoveMasks m = new MoveMasks();
 
         Board[] boards = {
-                new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w -"),
-                new Board("8/8/4r3/3R4/8/8/8/8 w -"),
-                new Board("8/8/4r3/3R4/8/8/8/8 b -"),
-                new Board("8/8/3Rr3/3rR3/8/8/8/8 w -"),
-                new Board("8/8/3Rr3/3rR3/8/8/8/8 b -"),
-                new Board("8/8/2r1R3/8/2r1R3/8/8/8 w -"),
-                new Board("8/8/2r1R3/8/2r1R3/8/8/8 b -"),
-                new Board("rnbqkbnr/1pppppp1/8/8/8/8/1PPPPPP1/1NBQKBN1 w -"),
-                new Board("1nbqkbn1/1pppppp1/8/8/8/8/1PPPPPP1/RNBQKBNR b -"),
+            new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w -"),
+            new Board("8/8/4r3/3R4/8/8/8/8 w -"),
+            new Board("8/8/4r3/3R4/8/8/8/8 b -"),
+            new Board("8/8/3Rr3/3rR3/8/8/8/8 w -"),
+            new Board("8/8/3Rr3/3rR3/8/8/8/8 b -"),
+            new Board("8/8/2r1R3/8/2r1R3/8/8/8 w -"),
+            new Board("8/8/2r1R3/8/2r1R3/8/8/8 b -"),
+            new Board("rnbqkbnr/1pppppp1/8/8/8/8/1PPPPPP1/1NBQKBN1 w -"),
+            new Board("1nbqkbn1/1pppppp1/8/8/8/8/1PPPPPP1/RNBQKBNR b -"),
         };
 
         Move[][] results = {
-                generateTestMoves(new long[] {0L, 0L}, new int[] {0, 7}, new PieceType[]{PieceType.ROOK, PieceType.ROOK}),
-                generateTestMoves(new long[] {578722409201797128L}, new int[] {35}, new PieceType[]{PieceType.ROOK}),
-                generateTestMoves(new long[] {1157687956502220816L}, new int[] {44}, new PieceType[]{PieceType.ROOK}),
-                generateTestMoves(new long[] {578737875244285952L, 18588887945232L}, new int[] {43, 36}, new PieceType[]{PieceType.ROOK, PieceType.ROOK}),
-                generateTestMoves(new long[] {8895012014088L, 1157680259651338240L}, new int[] {35, 44}, new PieceType[]{PieceType.ROOK, PieceType.ROOK}),
-                generateTestMoves(new long[] {1157684657697849344L, 72679952400L}, new int[] {44, 28}, new PieceType[]{PieceType.ROOK, PieceType.ROOK}),
-                generateTestMoves(new long[] {289385980052373504L, 17633117188L}, new int[] {42, 26}, new PieceType[]{PieceType.ROOK, PieceType.ROOK}),
-                generateTestMoves(new long[] {0L, 0L}, new int[] {0, 0}, new PieceType[]{PieceType.ROOK, PieceType.ROOK}),
-                generateTestMoves(new long[] {0L, 0L}, new int[] {0, 0}, new PieceType[]{PieceType.ROOK, PieceType.ROOK}),
+            generateTestMoves(new long[] {0L, 0L}, new int[] {0, 7}, new PieceType[]{PieceType.ROOK, PieceType.ROOK}),
+            generateTestMoves(new long[] {578722409201797128L}, new int[] {35}, new PieceType[]{PieceType.ROOK}),
+            generateTestMoves(new long[] {1157687956502220816L}, new int[] {44}, new PieceType[]{PieceType.ROOK}),
+            generateTestMoves(new long[] {578737875244285952L, 18588887945232L}, new int[] {43, 36}, new PieceType[]{PieceType.ROOK, PieceType.ROOK}),
+            generateTestMoves(new long[] {8895012014088L, 1157680259651338240L}, new int[] {35, 44}, new PieceType[]{PieceType.ROOK, PieceType.ROOK}),
+            generateTestMoves(new long[] {1157684657697849344L, 72679952400L}, new int[] {44, 28}, new PieceType[]{PieceType.ROOK, PieceType.ROOK}),
+            generateTestMoves(new long[] {289385980052373504L, 17633117188L}, new int[] {42, 26}, new PieceType[]{PieceType.ROOK, PieceType.ROOK}),
+            generateTestMoves(new long[] {0L, 0L}, new int[] {0, 0}, new PieceType[]{PieceType.ROOK, PieceType.ROOK}),
+            generateTestMoves(new long[] {0L, 0L}, new int[] {0, 0}, new PieceType[]{PieceType.ROOK, PieceType.ROOK}),
         };
 
         for (int i = 0; i < results.length; i++) {
@@ -52,27 +52,27 @@ public class MoveGeneratorTest {
         MoveMasks m = new MoveMasks();
 
         Board[] boards = {
-                new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w -"),
-                new Board("8/8/3b4/8/3B4/8/8/8 w -"),
-                new Board("8/8/3b4/8/3B4/8/8/8 b -"),
-                new Board("8/8/4bb2/8/3B2B1/8/8/8 w -"),
-                new Board("8/8/4bb2/8/3B2B1/8/8/8 b -"),
-                new Board("8/1p5p/2P3p1/8/4B3/8/2p3P1/1p5p w -"),
-                new Board("8/1p5p/2P3p1/8/4b3/8/2p3P1/1p5p b -"),
-                new Board("rnbqkbnr/pppppppp/8/8/8/8/P1P2P1P/RN1QK1NR w -"),
-                new Board("rn1qk1nr/p1p2p1p/8/8/8/8/PPPPPPPP/RNBQKBNR b -"),
+            new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w -"),
+            new Board("8/8/3b4/8/3B4/8/8/8 w -"),
+            new Board("8/8/3b4/8/3B4/8/8/8 b -"),
+            new Board("8/8/4bb2/8/3B2B1/8/8/8 w -"),
+            new Board("8/8/4bb2/8/3B2B1/8/8/8 b -"),
+            new Board("8/1p5p/2P3p1/8/4B3/8/2p3P1/1p5p w -"),
+            new Board("8/1p5p/2P3p1/8/4b3/8/2p3P1/1p5p b -"),
+            new Board("rnbqkbnr/pppppppp/8/8/8/8/P1P2P1P/RN1QK1NR w -"),
+            new Board("rn1qk1nr/p1p2p1p/8/8/8/8/PPPPPPPP/RNBQKBNR b -"),
         };
 
         Move[][] results = {
-                generateTestMoves(new long[] {0L, 0L}, new int[] {2, 5}, new PieceType[]{PieceType.BISHOP, PieceType.BISHOP}),
-                generateTestMoves(new long[] {-9205038694072573375L}, new int[] {27}, new PieceType[]{PieceType.BISHOP}),
-                generateTestMoves(new long[] {2455587783297826816L}, new int[] {43}, new PieceType[]{PieceType.BISHOP}),
-                generateTestMoves(new long[] {318944272720449L, 18279391301640L}, new int[] {27, 30}, new PieceType[]{PieceType.BISHOP, PieceType.BISHOP}),
-                generateTestMoves(new long[] {-8624392940535414784L, 4911175566587199744L}, new int[] {45, 44}, new PieceType[]{PieceType.BISHOP, PieceType.BISHOP}),
-                generateTestMoves(new long[] {70540545491968L}, new int[] {28}, new PieceType[]{PieceType.BISHOP}),
-                generateTestMoves(new long[] {4569847840768L}, new int[] {28}, new PieceType[]{PieceType.BISHOP}),
-                generateTestMoves(new long[] {0L, 0L}, new int[] {0, 0}, new PieceType[]{PieceType.BISHOP, PieceType.BISHOP}),
-                generateTestMoves(new long[] {0L, 0L}, new int[] {0, 0}, new PieceType[]{PieceType.BISHOP, PieceType.BISHOP}),
+            generateTestMoves(new long[] {0L, 0L}, new int[] {2, 5}, new PieceType[]{PieceType.BISHOP, PieceType.BISHOP}),
+            generateTestMoves(new long[] {-9205038694072573375L}, new int[] {27}, new PieceType[]{PieceType.BISHOP}),
+            generateTestMoves(new long[] {2455587783297826816L}, new int[] {43}, new PieceType[]{PieceType.BISHOP}),
+            generateTestMoves(new long[] {318944272720449L, 18279391301640L}, new int[] {27, 30}, new PieceType[]{PieceType.BISHOP, PieceType.BISHOP}),
+            generateTestMoves(new long[] {-8624392940535414784L, 4911175566587199744L}, new int[] {45, 44}, new PieceType[]{PieceType.BISHOP, PieceType.BISHOP}),
+            generateTestMoves(new long[] {70540545491968L}, new int[] {28}, new PieceType[]{PieceType.BISHOP}),
+            generateTestMoves(new long[] {4569847840768L}, new int[] {28}, new PieceType[]{PieceType.BISHOP}),
+            generateTestMoves(new long[] {0L, 0L}, new int[] {0, 0}, new PieceType[]{PieceType.BISHOP, PieceType.BISHOP}),
+            generateTestMoves(new long[] {0L, 0L}, new int[] {0, 0}, new PieceType[]{PieceType.BISHOP, PieceType.BISHOP}),
         };
 
         for (int i = 0; i < results.length; i++) {
@@ -85,27 +85,27 @@ public class MoveGeneratorTest {
         MoveMasks m = new MoveMasks();
 
         Board[] boards = {
-                new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w -"),
-                new Board("8/8/8/4q3/2Q5/8/8/8 w -"),
-                new Board("8/8/8/4q3/2Q5/8/8/8 b -"),
-                new Board("8/8/8/P1p1p3/8/P1Q1P3/8/p1p1P3 w -"),
-                new Board("8/8/8/P1p1p3/8/P1q1P3/8/p1p1P3 b -"),
-                new Board("rn1qkbnr/pp3ppp/3p4/8/8/8/PP3PPP/RN1QKBNR w -"),
-                new Board("rn1qkbnr/pp3ppp/3p4/8/8/8/PP3PPP/RN1QKBNR b -"),
-                new Board("rnbqkbnr/pppppppp/8/8/8/8/PP3PPP/RN2KBNR w -"),
-                new Board("rn2kbnr/pp3ppp/8/8/8/8/PPPPPPPP/RNBQKBNR b -"),
+            new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w -"),
+            new Board("8/8/8/4q3/2Q5/8/8/8 w -"),
+            new Board("8/8/8/4q3/2Q5/8/8/8 b -"),
+            new Board("8/8/8/P1p1p3/8/P1Q1P3/8/p1p1P3 w -"),
+            new Board("8/8/8/P1p1p3/8/P1q1P3/8/p1p1P3 b -"),
+            new Board("rn1qkbnr/pp3ppp/3p4/8/8/8/PP3PPP/RN1QKBNR w -"),
+            new Board("rn1qkbnr/pp3ppp/3p4/8/8/8/PP3PPP/RN1QKBNR b -"),
+            new Board("rnbqkbnr/pppppppp/8/8/8/8/PP3PPP/RN2KBNR w -"),
+            new Board("rn2kbnr/pp3ppp/8/8/8/8/PPPPPPPP/RNBQKBNR b -"),
         };
 
         Move[][] results = {
-                generateTestMoves(new long[] {0L}, new int[] {3}, new PieceType[]{PieceType.QUEEN}),
-                generateTestMoves(new long[] {4910072647826412836L}, new int[] {26}, new PieceType[]{PieceType.QUEEN}),
-                generateTestMoves(new long[] {-7902628846034972143L}, new int[] {36}, new PieceType[]{PieceType.QUEEN}),
-                generateTestMoves(new long[] {86134885893L}, new int[] {18}, new PieceType[]{PieceType.QUEEN}),
-                generateTestMoves(new long[] {4531621392L}, new int[] {18}, new PieceType[]{PieceType.QUEEN}),
-                generateTestMoves(new long[] {9381436070916L}, new int[] {3}, new PieceType[]{PieceType.QUEEN}),
-                generateTestMoves(new long[] {296149340215312384L}, new int[] {59}, new PieceType[]{PieceType.QUEEN}),
-                generateTestMoves(new long[] {0L}, new int[] {0}, new PieceType[]{PieceType.QUEEN}),
-                generateTestMoves(new long[] {0L}, new int[] {0}, new PieceType[]{PieceType.QUEEN}),
+            generateTestMoves(new long[] {0L}, new int[] {3}, new PieceType[]{PieceType.QUEEN}),
+            generateTestMoves(new long[] {4910072647826412836L}, new int[] {26}, new PieceType[]{PieceType.QUEEN}),
+            generateTestMoves(new long[] {-7902628846034972143L}, new int[] {36}, new PieceType[]{PieceType.QUEEN}),
+            generateTestMoves(new long[] {86134885893L}, new int[] {18}, new PieceType[]{PieceType.QUEEN}),
+            generateTestMoves(new long[] {4531621392L}, new int[] {18}, new PieceType[]{PieceType.QUEEN}),
+            generateTestMoves(new long[] {9381436070916L}, new int[] {3}, new PieceType[]{PieceType.QUEEN}),
+            generateTestMoves(new long[] {296149340215312384L}, new int[] {59}, new PieceType[]{PieceType.QUEEN}),
+            generateTestMoves(new long[] {0L}, new int[] {0}, new PieceType[]{PieceType.QUEEN}),
+            generateTestMoves(new long[] {0L}, new int[] {0}, new PieceType[]{PieceType.QUEEN}),
         };
 
         for (int i = 0; i < results.length; i++) {
@@ -155,4 +155,45 @@ public class MoveGeneratorTest {
         return true;
     }
 
+    @Test
+    void isAttacked() {
+        Board b = new Board("rnbqkbnr/pppp1ppp/8/8/8/8/PPPP1PPP/RNBQKBNR w - - 0 1");
+        assertFalse(MoveGenerator.isAttacked(b, new MoveMasks(), 4, BLACK));
+
+        b = new Board("rnb1kbnr/ppppqppp/8/8/8/8/PPPP1PPP/RNBQKBNR w - - 0 1");
+        assertTrue(MoveGenerator.isAttacked(b, new MoveMasks(), 4, BLACK));
+
+        b = new Board("rnb1kbnr/ppppqppp/8/8/8/4N3/PPPP1PPP/RNBQKB1R w - - 0 1");
+        assertFalse(MoveGenerator.isAttacked(b, new MoveMasks(), 4, BLACK));
+
+        b = new Board("4k3/8/8/8/8/3qn3/2r3p1/4K3 w - - 0 1");
+        assertFalse(MoveGenerator.isAttacked(b, new MoveMasks(), 4, BLACK));
+
+        b = new Board("7k/8/8/8/8/2B5/8/4K3 b - - 0 1");
+        assertTrue(MoveGenerator.isAttacked(b, new MoveMasks(), 63, WHITE));
+
+        b = new Board("7k/8/8/4P3/8/2B5/8/4K3 b - - 0 1");
+        assertFalse(MoveGenerator.isAttacked(b, new MoveMasks(), 63, WHITE));
+
+        b = new Board("7k/8/5n2/4P3/8/2B5/8/4K1R1 b - - 0 1");
+        assertFalse(MoveGenerator.isAttacked(b, new MoveMasks(), 63, WHITE));
+
+        b = new Board("6bk/5Nb1/5n2/4P3/8/2B5/8/4K1R1 b - - 0 1");
+        assertTrue(MoveGenerator.isAttacked(b, new MoveMasks(), 63, WHITE));
+
+        b = new Board("7k/6P1/8/8/8/2B5/8/4K3 b - - 0 1");
+        assertTrue(MoveGenerator.isAttacked(b, new MoveMasks(), 63, WHITE));
+    }
+
+    @Test
+    void generateKingMoves() {
+    }
+
+    @Test
+    void generateKnightMoves() {
+    }
+
+    @Test
+    void generatePawnMoves() {
+    }
 }
