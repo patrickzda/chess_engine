a7bd17ae412f7ca29968da5adc10fcf2e2cd0a54
patrickzda <81521650+patrickzda@users.noreply.github.com>
2023-05-06 12:31:10 +0200

    MoveGeneratorTest: KI spielt gegen sich selbst

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,25 @@ import engine.representation.PieceType;
 import performance.MoveGeneratorPerformance;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-       /* Board b = new Board("7k/7q/5pqp/6p1/NP3P1P/1KP3P1/2B5/8 b - - 0 1");
+        //String fens[] = new String[]{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq","4r1k1/1bqr1pbp/p2p2p1/4p1B1/2p1P3/PnP2N1P/BP2QPP1/3RR1K1 w KQkq","8/6k1/5bP1/4p2p/3pP2P/1b1qBK2/p1r5/6R1 b KQkq"};
+        //MoveGeneratorPerformance moveGeneratorPerformance = new MoveGeneratorPerformance();
+        //moveGeneratorPerformance.performanceMovegenratorOnBoards(fens,100000);
 
-        Move[] moves = MoveGenerator.generateLegalMoves(b, new MoveMasks());
+        Board board = new Board();
+        MoveMasks masks = new MoveMasks();
+        Random random = new Random();
+        int counter = 0;
 
-        System.out.println(moves.length);
-
-        for(int i = 0; i < moves.length; i++){
-            b.doMove(moves[i]);
-            System.out.println(b.toFENString());
-            b.undoLastMove();
-        }*/
-        String fens[] = new String[]{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w -","4r1k1/1bqr1pbp/p2p2p1/4p1B1/2p1P3/PnP2N1P/BP2QPP1/3RR1K1 w -","8/6k1/5bP1/4p2p/3pP2P/1b1qBK2/p1r5/6R1 b -"};
-        MoveGeneratorPerformance moveGeneratorPerformance = new MoveGeneratorPerformance();
-        moveGeneratorPerformance.performanceMovegenratorOnBoards(fens,100000);
+        Move[] legalMoves = MoveGenerator.generateLegalMoves(board, masks);
+        while(legalMoves.length > 0 && !board.isKingOfTheHill() && counter < 40){
+            board.doMove(legalMoves[random.nextInt(legalMoves.length)]);
+            System.out.println(board.toFENString());
+            legalMoves = MoveGenerator.generateLegalMoves(board, masks);
+            counter++;
+        }
     }
 }
