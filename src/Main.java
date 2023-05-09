3745d76c3148f6cbf1ef48b628f879ec2dc17535
unknown <adam.knothe@gmx.de>
2023-05-09 16:21:59 +0200

    perfomance time hübsch machen

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,11 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        //String fens[] = new String[]{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq","4r1k1/1bqr1pbp/p2p2p1/4p1B1/2p1P3/PnP2N1P/BP2QPP1/3RR1K1 w KQkq","8/6k1/5bP1/4p2p/3pP2P/1b1qBK2/p1r5/6R1 b KQkq"};
-        //MoveGeneratorPerformance moveGeneratorPerformance = new MoveGeneratorPerformance();
-        //moveGeneratorPerformance.performanceMovegeneratorOnBoards(fens,100000);
+        String fens[] = new String[]{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq","4r1k1/1bqr1pbp/p2p2p1/4p1B1/2p1P3/PnP2N1P/BP2QPP1/3RR1K1 w KQkq","8/6k1/5bP1/4p2p/3pP2P/1b1qBK2/p1r5/6R1 b KQkq"};
+        MoveGeneratorPerformance moveGeneratorPerformance = new MoveGeneratorPerformance();
+        moveGeneratorPerformance.measureAveragePerformanceOnBoards(fens, 100000);
 
-        Board board = new Board();
+      /*  Board board = new Board();
         MoveMasks masks = new MoveMasks();
         int counter = 0;
 
@@ -31,5 +31,7 @@ public class Main {
             }
             counter++;
         }
+
+       */
     }
 }
