722092726fa3c2f1d7cf47ce8976a5555b1f2df7
patrickzda <81521650+patrickzda@users.noreply.github.com>
2023-05-09 16:56:40 +0200

    Cleaner Code

--- src/Main.java
+++ src/Main.java
@@ -15,13 +15,11 @@ public class Main {
     public static void main(String[] args) {
         String fens[] = new String[]{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq","4r1k1/1bqr1pbp/p2p2p1/4p1B1/2p1P3/PnP2N1P/BP2QPP1/3RR1K1 w KQkq","8/6k1/5bP1/4p2p/3pP2P/1b1qBK2/p1r5/6R1 b KQkq"};
         MoveGeneratorPerformance moveGeneratorPerformance = new MoveGeneratorPerformance();
-        moveGeneratorPerformance.measureAveragePerformanceOnBoards(fens,100000);
+        moveGeneratorPerformance.measureAveragePerformanceOnBoards(fens,1000);
     }
 
     static void playAgainstItself(){
         Board board = new Board();
-
-      /*  Board board = new Board();
         MoveMasks masks = new MoveMasks();
         int counter = 0;
 
@@ -35,7 +33,6 @@ public class Main {
             }
             counter++;
         }
-
-       */
     }
+
 }
