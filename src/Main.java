ad94143a231cfe4ce0233a3e36095a94cd83705c
patrickzda <81521650+patrickzda@users.noreply.github.com>
2023-06-07 11:16:48 +0200

    Weitere Tests

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,34 @@ import engine.move_generation.MoveGenerator;
 import engine.move_generation.MoveMasks;
 import engine.representation.*;
 import performance.*;
+import test.FENData;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        //performance();
-
         //playMove(args);
+        measureAverageTimeOnFENData(4, 0, 25);
+    }
 
-        playTimed(new String[]{"2k5/6q1/3P1P2/4N3/8/1K6/8/8 w - - 0 1", "4r1k1/1bqr1pbp/p2p2p1/4p1B1/2p1P3/PnP2N1P/BP2QPP1/3RR1K1 w Qq - 0 1", "6k1/r4ppp/r7/1b6/8/8/4QPPP/4R1K1 w - - 0 1"}, 5000);
+    static void measureAverageTimeOnFENData(int depth, int startIndex, int endIndex){
+        long averageTime = 0;
+        MoveMasks masks = new MoveMasks();
+        for(int i = startIndex; i < endIndex; i++){
+            String currentFEN = FENData.FEN_DATA[i] + " 0 1";
+            averageTime = averageTime + FENData.getTimeOfDepth(new Board(currentFEN), masks, depth);
+        }
+        System.out.println((averageTime / (endIndex - startIndex)) / 1000000 + " ms");
+    }
 
+    static void measureAverageDepthOnFENData(int millisPerSearch, int startIndex, int endIndex){
+        int averageDepth = 0;
+        MoveMasks masks = new MoveMasks();
+        for(int i = startIndex; i < endIndex; i++){
+            String currentFEN = FENData.FEN_DATA[i] + " 0 1";
+            averageDepth = averageDepth + FENData.getDepthOfBestMoveTimed(new Board(currentFEN), masks, millisPerSearch);
+        }
+        System.out.println(averageDepth / (endIndex - startIndex));
     }
 
     static void performance(){
